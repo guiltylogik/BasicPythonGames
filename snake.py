81cd453483db4559e53d8eb4ea7811948c8325c9
Jaspar Ifechi <guiltylogik@gmail.com>
2019-11-11 21:10:29 +0000

    now started on a snake game

---
+++ snake.py
@@ -0,0 +1,56 @@
+"""
+    A simple Snake game in Python.
+    by guiltylogik
+"""
+
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox as msg
+
+
+class cube(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+class make(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def drawGrid(w, rows, surface):
+    pass
+
+def redrawWindow(surface):
+    pass
+
+def randomSnack(rows, items):
+    pass
+
+def messageBox(subject, content):
+    pass
+
+def main():
+    pass
