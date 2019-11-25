98fc64a4ee832f0df01e5ae6c947a66a85a6f0fe
Jaspar Ifechi <guiltylogik@gmail.com>
2019-11-25 16:13:07 +0000

    Implemented shape formats, checking for valid spaces, check loosing and fixed minor errors. Basically, completed basic game play.

--- tetris.py
+++ tetris.py
@@ -143,6 +143,7 @@ class Piece(object):
     def __init__(self, x, y, shape):
             self.x = x
             self.y = y
+            self.shape = shape
             self.color = shape_colors[shapes.index(shape)]
             self.rotation = 0
 
@@ -157,13 +158,38 @@ def create_grid(locked_positions={}):
     return grid
 
 def convert_shape_format(shape):
-    pass
+    positions = []
+    shape_format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(shape_format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
 
 def valid_space(shape, grid):
-    pass
+    valid_pos = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
+    valid_pos = [j for sub in valid_pos for j in sub]
+
+    formatted = convert_shape_format(shape)
+
+    for pos in formatted:
+        if pos not in valid_pos:
+            if pos[1] > -1:
+                return False
+    return True
 
 def check_lost(positions):
-    pass
+    for pos in positions:
+        x, y = pos
+        if y < 1:
+            return True
+    return False
 
 def get_shape():
     return Piece(5, 0, random.choice(shapes))
@@ -216,8 +242,20 @@ def main(win):
     next_piece = get_shape()
     clock = pygame.time.Clock()
     fall_time = 0
+    fall_speed = 0.27
 
     while run:
+        grid = create_grid(locked_pos)
+        fall_time += clock.get_rawtime()
+        clock.tick()
+
+        if fall_time/1000 > fall_speed:
+            fall_time = 0
+            current_piece.y += 1
+            if not(valid_space(current_piece, grid)) and current_piece.y > 0:
+                current_piece.y -= 1
+                change_piece = True
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -226,11 +264,11 @@ def main(win):
                 if event.key == pygame.K_LEFT:
                     current_piece.x -= 1
                     if not(valid_space(current_piece, grid)):
-                        current_piece += 1
+                        current_piece.x += 1
                 if event.key == pygame.K_RIGHT:
                     current_piece.x += 1
                     if not(valid_space(current_piece, grid)):
-                        current_piece -= 1
+                        current_piece.x -= 1
                 if event.key == pygame.K_DOWN:
                     current_piece.y += 1
                     if not(valid_space(current_piece, grid)):
@@ -238,14 +276,34 @@ def main(win):
                 if event.key == pygame.K_UP:
                     current_piece.rotation += 1
                     if not(valid_space(current_piece, grid)):
-                        current_piece -= 1
+                        current_piece.rotation -= 1
 
-    draw_window(win, grid)
+        shape_pos = convert_shape_format(current_piece)
+
+        for i in range(len(shape_pos)):
+            x, y = shape_pos[i]
+            if y > -1:
+                grid[y][x] = current_piece.color
+
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0], pos[1])
+                locked_pos[p] = current_piece.color
+            current_piece = next_piece
+            next_piece = get_shape()
+            change_piece = False
+
+        draw_window(win, grid)
+
+        if check_lost(locked_pos):
+            run = False
+
+    pygame.display.quit()
 
 def main_menu(win):
       main(win)
 
 win = pygame.display.set_mode((s_width, s_height))
-pygame.display.set_caption("Tetris")
+pygame.display.set_caption("Tetris By Guiltylogik")
 
 main_menu(win)  # start game
