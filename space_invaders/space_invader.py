4bc7688f53208b760a187848b3c3b7866f7e30d8
Jaspar Ifechi <guiltylogik@gmail.com>
2019-11-29 01:13:55 +0000

    trying out the turtle module

--- space_invaders/space_invader.py
+++ space_invaders/space_invader.py
@@ -1,1 +1,43 @@
 # Space Invaders
+
+import turtle
+
+pen = turtle.Turtle()
+
+pen.color('red')
+pen.pendown()
+for s in range(3):
+    pen.forward(200)
+    pen.right(90)
+pen.forward(200)
+
+player = turtle.Turtle()
+player.penup()
+
+def move_right():
+    x = player.xcor()
+    x += 10
+    player.setx(x)
+
+def move_left():
+    x = player.xcor()
+    x -= 10
+    player.setx(x)
+
+def move_forward():
+    y = player.ycor()
+    y += 10
+    player.sety(y)
+
+def move_backward():
+    y = player.ycor()
+    y -= 10
+    player.sety(y)
+
+win = turtle.Screen()
+win.listen()
+
+win.onkey(move_right, 'd')
+win.onkey(move_left, 'a')
+win.onkey(move_forward, 'w')
+win.onkey(move_backward, 's')
